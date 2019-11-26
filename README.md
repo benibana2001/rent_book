f1d4d98d5a93ebf50032348d061d61a93dfdca1b
benibana2001 <benibana2001@gmail.com>
2019-11-26 12:19:57 +0900

    Add mock file

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
+## React
+
 [こちら](https://ja.reactjs.org/docs/static-type-checking.html#typescript)を使用してプロジェクトを初期化しています。
 
 ## API
 こちらのAPIを使用しています。
 [図書館API仕様書 | カーリル](https://calil.jp/doc/api_ref.html)
+
+### 蔵書検索
+> 実行例：
+http://api.calil.jp/check?appkey={あなたのアプリキー}&isbn=4834000826&systemid=Aomori_Pref&format=json
+
+### 図書館データベース
+> 実行例：
+http://api.calil.jp/library?appkey={あなたのアプリキー}&pref=埼玉県
+http://api.calil.jp/library?appkey={あなたのアプリキー}&geocode=136.7163027,35.390516&limit=10
--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 const App: React.FC = () => {
     console.log(`process.env.NODE_ENV is: ${process.env.NODE_ENV}`)
     console.log(`REACT_APP_API_KEY is: ${process.env.REACT_APP_API_KEY}`)
+    getData()
     return (
         <div className="App">
             <header className="App-header">
@@ -23,6 +24,21 @@ const App: React.FC = () => {
             </header>
         </div>
     );
+}
+
+let getData = async (): Promise<any> => {
+    // TODO: Return value is possible to be JSONP, so that using XMLHttpRequest might be better way.
+    // let url: string = 'https://api.calil.jp/library?appkey=' + process.env.REACT_APP_API_KEY + '&geocode=136.7163027,35.390516&limit=10'
+    let url: string = 'https://api.calil.jp/library?appkey=46a2412f4ceb07b72a251150f2533c74&geocode=136.7163027,35.390516&limit=10'
+    // let url: string = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+    console.log(url)
+    let response: any = await fetch(url, {
+        method: "GET",
+        mode: "cors",
+        credentials: "include"
+    })
+    console.log(response)
+    return response
 }
 
 export default App;
