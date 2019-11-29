5b57d2277efcc86f9bc9407ccad672222b5dca38
benibana2001 <benibana2001@gmail.com>
2019-11-29 12:33:22 +0900

    Edit Project to stop using create-react-app package.

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,75 @@
-import React from 'react';
-import { Calil, options } from './Calil'
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+export { View }
 
-const App: React.FC = () => {
-    console.log(`process.env.NODE_ENV is: ${process.env.NODE_ENV}`)
-    console.log(`REACT_APP_API_KEY is: ${process.env.REACT_APP_API_KEY}`);
+class Button extends React.Component<{ value: string, name: string }> {
+  private log = (): void => {
+    console.log(this.props.name)
+    console.log(process.env.NODE_ENV)
+  }
 
-    (async () => {
-        let o: options = {
-            'appkey': process.env.REACT_APP_API_KEY as string,
-            'isbn': [111],
-            'systemid': [111]
-        }
-        // 
-        let c: Calil = new Calil(o)
-        c.search()
-    })()
+  getTopStoryJSON = async (): Promise<any> => {
+    const HKN_TOP_URL: string = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+    let response: any = await fetch(HKN_TOP_URL)
+    return response.json()
+  }
 
+  jsonValid = async(): Promise<any> => {
+    let json: any = await this.getTopStoryJSON()
+    console.log(json)
+    console.log(typeof(json))
+  }
+
+  render() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Edit <code>src/App.tsx</code> and save to reload.</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+      <ul>
+        <li><button onClick={this.log}>{this.props.value}</button></li>
+        <li><button onClick={this.jsonValid}>JSON</button></li>
+      </ul>
+    )
+  }
+}
+
+class Card extends React.Component<{ name: string }>{
+  render() {
+    return (
+      <ul>
+        <li>{this.props.name}</li>
+        <Button value="OK" name={this.props.name} />
+      </ul>
+    )
+  }
+}
+
+class View extends React.Component {
+  render() {
+    return (
+      <div>
+        <Card name="Taro" />
+        <Card name="Alex" />
+        <Card name="Bob" />
+      </div>
+    )
+  }
 }
 
 
-
-export default App;
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    plugins: [
+        new webpack.ProgressPlugin(), 
+        new HtmlWebpackPlugin({
+            template: './src/html/index.html'
+        })],
+
+    module: {
+        rules: [
+            {
+                test: /.(ts|tsx)$/,
+                loader: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')],
+                exclude: [/node_modules/]
+            },
+            {
+				test: /\.html$/,
+				include: [path.resolve(__dirname, 'src')],
+				loader: "html-loader"
+			}
+        ]
+    },
+
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendors: {
+                    priority: -10,
+                    test: /[\\/]node_modules[\\/]/
+                }
+            },
+
+            chunks: 'async',
+            minChunks: 1,
+            minSize: 30000,
+            name: true
+        }
+    },
+
+    devServer: {
+        open: true
+    },
+
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    }
+};
