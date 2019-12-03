27fd4a14a10dc070bac4f2a6a40d13b7018f81ef
benibana2001 <benibana2001@gmail.com>
2019-12-03 22:08:07 +0900

    Implementing Calil client

--- src/Calil.ts
+++ src/Calil.ts
@@ -7,10 +7,21 @@ class Calil {
     public api_call_count: number = 0
     public data_cache: string = ''
     //
-    public systemid_list: number[] = []
-    public isbn_list: number[] = []
+    private readonly HOST: string = 'https://api.calil.jp/check'
     //
     private _options: options
+    //
+    private _server_status: number = 0
+    set server_status(status: number) { this._server_status = status }
+    get server_status() { return this._server_status }
+    //
+    private _session: string = ''
+    set session(session: string) { this._session = session }
+    get session() { return this._session }
+    //
+    private _data: dataRow[] = []
+    set data(data: dataRow[]) { this._data = data }
+    get data() { return this._data }
     //----------------------------------------
     constructor(O: options) {
         // Set Arguments.
@@ -22,7 +33,7 @@ class Calil {
     /**
      * init
      */
-    public init(): void {
+    private init(): void {
         this.checkOptions()
         // THIS IS DEBUG FUNCTION TO BE ERASED!!
         // this.setTestOptions()
@@ -41,14 +52,19 @@ class Calil {
      * checkOptions
      * Validate Options each value.
      */
-    public checkOptions(): void {
+    private checkOptions(): void {
         // Set AppKey
         if (!this._options.appkey) {
             alert('Please enter appkey')
         }
         // Set ISBN to property.
-
+        /**
+         * ISBN would be passed as string everytime.
+         */
         // Set SystemID to property.
+        /**
+         * User wouldn't enter SystemID by theirself, and SystemID would be assumed inputted by App, so omit validataion.
+         */
     }
     /**
      * search
@@ -66,22 +82,90 @@ class Calil {
         // https://api.calil.jp/check?appkey={}&isbn=4334926940&systemid=Tokyo_Setagaya&format=json
         // https://api.calil.jp/check?appkey={}&isbn=4834000826&systemid=Aomori_Pref&format=json
         let url: string = (
-            'https://api.calil.jp/check' +
+            this.HOST +
             '?appkey=' + this._options.appkey +
             '&isbn=' + this._options.isbn +
             '&systemid=' + this._options.systemid + '&format=json')
         console.log(url)
         // Request
         let json: any = await this.callApi(url)
-        // Check data
-        let status: number = this.checkServerStatus(json)
+        // Check server status
+        /**
+         * Calis server return status as a number.
+         * We should judge server process would be done or not by checking this status.
+         * If not finished, we should proceed polling process.
+         */
+        await this.checkServerStatus(json)
         // Check value 'continue' and decide next process.
-        this.confirm(status)
+        // this.confirm()
         // DONE
         console.log('search() is finished.')
         // Parse Data
-        return this.parse(json)
+        return this._data
     }
+    /**
+     * checkServerStatus
+     */
+    private async checkServerStatus(json: any): Promise<any> {
+        // Set server status
+        this.server_status = this.getServerStatus(json)
+        // Set session
+        this.session = this.getSession(json)
+        //
+        if (this.server_status === 1) {
+            // Polling
+            await this.sleep(1000)
+            await this.poll()
+
+        } else if (this.server_status === 0) {
+            // Done
+            // Parse
+            const data: dataRow[] = this.parse(json)
+            // Set data
+            this.data = data
+            return
+
+        } else {
+            console.log(`Error - server.status: ${this.server_status}`)
+            return 
+        }
+    }
+    /**
+     * poll
+     */
+    public async poll(): Promise<any> {
+        console.log('Start polling.')
+        const url: string = (
+            this.HOST +
+            '?appkey=' + this._options.appkey +
+            '&session=' + this._session + '&format=json')
+        // request polling
+        let json: any = await this.callApi(url)
+        console.log(`Polling url: ${url}`)
+        console.log(`Fetch from polling: ${JSON.stringify(json)}`)
+        // Check server status
+        this.checkServerStatus(json)
+    }
+    /**
+     * confirm
+     * Check server process was done or not, and proceed next process.
+     */
+    private async confirm(): Promise<any> {
+        if (this.server_status === 1) {
+            // AB-NORMAL. If data.continue === 1, server is still running
+            // Do polling. This function should be loop until server process done.
+            await this.sleep(1000)
+            await this.poll()
+            return
+        } else if (this.server_status === 0) {
+            // NORMAL. If data.continue === 0, server process is done.
+            // console.log('Serve processe is done.')
+            return
+        } else {
+            // console.log(`json.continue is wrong value.`)
+        }
+    }
+
     /**
      * parse
      * 
@@ -89,22 +173,16 @@ class Calil {
      * When display some data in JSX, you are recommended to use Array.map() function, 
      * and raw JSON data is difficult to display. So that this function parse JSON data to array.
      */
-    public parse (json: any):  dataRow[] {
+    public parse(json: any): dataRow[] {
         let libkey: any = json.books[this._options.isbn][this._options.systemid].libkey
         let datas: dataRow[] = []
         let i: number = 1
-        for(let key in libkey){
-            let d: dataRow = {id: i, name: key, status: libkey[key]}
+        for (let key in libkey) {
+            let d: dataRow = { id: i, name: key, status: libkey[key] }
             datas.push(d)
             i++
         }
         return datas
-    }
-    /**
-    * poll
-    */
-    public poll(): void {
-        console.log('Start polling.')
     }
     /**
      * callApi
@@ -117,29 +195,23 @@ class Calil {
     }
 
     /**
-     * checkServerStatus
+     * getServerStatus
      */
-    private checkServerStatus(data: any): number {
+    private getServerStatus(data: any): number {
         return data.continue
     }
     /**
-     * confirm
-     * Check All data was got or not.
-     * And Do next process.
+     * getSession
      */
-    private confirm(status: number): void {
-        if (status === 1) {
-            // AB-NORMAL. If data.continue === 1, server is still running
-            // console.log('Server is still runnning. So start polling...')
-            // Do polling. This function should be loop until server process done.
-            this.poll()
-        } else if (status === 0) {
-            // NORMAL. If data.continue === 0, server process is done.
-            // console.log('Serve processe is done.')
-            return
-        } else {
-            // console.log(`json.continue is wrong value.`)
-        }
+    private getSession (data: any):  string {
+        return data.session
+    }
+
+    /**
+     * sleep
+     */
+    private sleep(ms: number): Promise<any> {
+        return new Promise(resolve => setTimeout(resolve, ms))
     }
 }
 
