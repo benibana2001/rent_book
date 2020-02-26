bc6316dbf2dcf474a5bea099884b4dc4a25bdd72
benibana2001 <benibana2001@gmail.com>
2020-02-26 18:14:21 +0900

    [2020-0226/rent_books] Add Toast test

---
+++ src/test/Toast.test.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import * as ReactDOM from 'react-dom'
+import { unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import Toast from '../components/Common/Toast'
+
+let container: HTMLElement | null = null
+beforeEach(() => {
+    container = document.createElement('div')
+    document.body.appendChild(container)
+})
+
+afterEach(() => {
+    unmountComponentAtNode(container)
+    container.remove()
+    container = null
+})
+
+it(('render'), () => {
+    act(() => {
+        ReactDOM.render(<Toast text={'Hello world!'} />, container)
+    })
+    const outerElem: HTMLElement = document.getElementsByClassName('outer')[0] as HTMLElement
+    expect(outerElem.textContent).toEqual('Hello world!')
+})
