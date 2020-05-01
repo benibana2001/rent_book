aba3217359ee7452586791c1e755f40e75b56bd4
benibana2001 <benibana2001@gmail.com>
2020-05-01 14:48:06 +0900

    [2020-05-02/rent_book] edit eslint plugin, rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,25 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-      '@typescript-eslint',
-      'react'
-    ],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/eslint-recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier/@typescript-eslint',
-    ],
-    rules: {
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/interface-name-prefix": 0,
-        "@typescript-eslint/no-use-before-define": 0,
-        "@typescript-eslint/no-var-requires": 0,
-        "react/jsx-uses-vars": 1
-
-    },
-    env: {
-        "browser": true,
-        "node": true,
-        "commonjs": true
-    }
-  };
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/interface-name-prefix': 0,
+    '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    'react/jsx-uses-vars': 1,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+  },
+}
